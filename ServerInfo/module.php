b92b156b017ff865f00df7e4140cd32e92fc0f80
demel42 <demel42@web.de>
2018-08-14 16:05:45 +0000

    Apply fixes from StyleCI

--- ServerInfo/module.php
+++ ServerInfo/module.php
@@ -373,8 +373,9 @@ class ServerInfo extends IPSModule
     {
         for ($cnt = 0; $cnt < 2; $cnt++) {
             $device = $this->ReadPropertyString('partition' . $cnt . '_device');
-			if ($device == '')
-				continue;
+            if ($device == '') {
+                continue;
+            }
 
             $Mountpoint = '';
             $Size = 0;
@@ -448,8 +449,9 @@ class ServerInfo extends IPSModule
     {
         for ($cnt = 0; $cnt < 2; $cnt++) {
             $device = $this->ReadPropertyString('disk' . $cnt . '_device');
-			if ($device == '')
-				continue;
+            if ($device == '') {
+                continue;
+            }
 
             $res = $this->execute('hddtemp ' . $device);
             if ($res == '' || count($res) < 1) {
