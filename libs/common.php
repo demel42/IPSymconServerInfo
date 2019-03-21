9bacd12a2a47daaf47b2d54ec552c1fae8c16a73
demel42 <demel42@web.de>
2019-03-21 19:35:23 +0000

    Apply fixes from StyleCI

--- libs/common.php
+++ libs/common.php
@@ -17,7 +17,7 @@ trait ServerInfoCommon
             return;
         }
 
-		$ret = parent::SetValue($Ident, $Value);
+        $ret = parent::SetValue($Ident, $Value);
         if ($ret == false) {
             $this->SendDebug(__FUNCTION__, 'mismatch of value "' . $Value . '" for variable ' . $Ident, 0);
         }
@@ -31,7 +31,7 @@ trait ServerInfoCommon
             return false;
         }
 
-		$ret = parent::GetValue($Ident);
+        $ret = parent::GetValue($Ident);
         return $ret;
     }
 
